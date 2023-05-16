bc04bdeb480ee258f2f421d32fe53ca48d67bcc5
Bmore0fm3 <36344386+bmore0fm3@users.noreply.github.com>
2023-05-16 15:18:12 -0400

    Update mini project
    JavaScript mini project to count from 0 to 100

--- js/site.js
+++ js/site.js
@@ -1,4 +1,59 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+// get the values from the page
+//Starts or controller function
+function getValues (){
+     //get values from the page 
+     let startValue = document.getElementById("startValue").value;
+     let endValue = document.getElementById("endValue").value;
+
+
+     //parse strings into ints
+     startValue = parseInt(startValue);
+     endValue = parseInt(endValue);
+
+     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        // call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+
+        //call display numbers
+        displayNumbers(numbers);
+
+     }else {
+        alert("Number must be an Integer");
+    
+     }
+
+}
+
+//generate numbers from start value to end value
+//logic functions()
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+    for (let index = startValue; index <= endValue; index++) {
+        numbers.push(index);  
+    }
+
+    return numbers;
+}
+
+//display the numbers and mark even numbers in bold
+//display or view functions
+let templateRows = "";
+function displayNumbers(numbers){
+    for (let index = 0; index < numbers.length; index++) {
+    
+        let number = numbers[index];
+        let className = "even";
+        
+        if(number % 2 === 0){
+            className = "even";
+
+        } else {
+            className = "odd";
+
+       }
+       templateRows += `<tr><td class=${className}>${number}</td></tr>`
+        
+    }
+    document.getElementById("results").innerHTML = templateRows;
+
 }
